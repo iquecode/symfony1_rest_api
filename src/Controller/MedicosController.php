79a58af784096c76098e0525f83d762fbc3ffa91
iquecode <iquecode@gmail.com>
2022-11-14 15:49:51 -0300

    doctrime and db

---
+++ src/Controller/MedicosController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Medico;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MedicosController
+{
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/medicos", methods={"POST"})
+     */
+    public function novo(Request $request): Response
+    {
+        $corpoRequisicao = $request->getContent();
+        $dadoEmJson = json_decode($corpoRequisicao);
+
+        $medico = new Medico();
+        $medico->crm = $dadoEmJson->crm;
+        $medico->nome = $dadoEmJson->nome;
+
+        $this->entityManager->persist($medico);
+        $this->entityManager->flush();
+
+
+        return new JsonResponse($medico);
+    }
+}
---
+++ src/Entity/Medico.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class Medico
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    public int $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    public int $crm;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    public string $nome;
+}
